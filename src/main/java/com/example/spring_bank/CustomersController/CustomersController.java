ce61de36081062d078bb8fcc08cf408aa612cd04
mohammed <malajaji.97@gmail.com>
2023-05-17 07:34:29 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_bank/CustomersController/CustomersController.java
@@ -0,0 +1,2 @@
+package com.example.spring_bank.CustomersController;public class CustomersController {
+}
---
+++ src/main/java/com/example/spring_bank/Model/Customer.java
@@ -0,0 +1,7 @@
+package com.example.spring_bank.Model;
+
+public class Customers {
+
+    private String id, username;
+    private double balance;
+}
